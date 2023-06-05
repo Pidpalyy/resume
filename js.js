f85b795b92d7da5cfd249b844094b3e47694a072
dima2282222 <113624834+dima2282222@users.noreply.github.com>
2023-06-05 23:16:06 +0300

    js.js

--- js.js
+++ js.js
@@ -2,3 +2,29 @@
 $(function(){
     $('.main_title').slideUp(0).slideDown(2000);
 });
+
+$(function(){
+    $('.background_slide_html').hide(0).show(5000);
+});
+
+$(function(){
+    $('.background_slide_css').hide(0).show(5000);
+});
+
+$(function(){
+    $('.background_slide_js').hide(0).show(5000);
+});
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.what');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
